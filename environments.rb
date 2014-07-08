19b828dcf01f805f65a53bacbc8ecddd14654fc7
Dan LaMee <dan@Magrathea.local>
2014-07-07 20:57:13 -0400

    update environments.rb

--- environments.rb
+++ environments.rb
@@ -1,4 +1,5 @@
 configure :development do
+	DataMapper
 	set :database, 'sqlite3:dev.db'
 	set :show_exceptions, true
 end
@@ -7,7 +8,7 @@ configure :production do
 	db = URI.parse(ENV['HEROKU_POSTGRESQL_GOLD_URL'])
 
 	ActiveRecord::Base.establish_connection(
-		:adapter => db.scheme == 'postgres' ? 'postgressql' : db.scheme,
+		:adapter => 'postgressql',
 		:host => db.host,
 		:username => db.user,
 		:password => db.password,

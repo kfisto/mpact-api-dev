54b099b369b1f742f5f95fb8db6911f78a8c2810
Dan LaMee <dan.lamee@gmail.com>
2015-10-05 13:14:00 -0400

    adjust pg entry requests query

--- mpact_api.rb
+++ mpact_api.rb
@@ -100,12 +100,12 @@ helpers do
 	end
 
 	def entry_requests
-		@entry_requests ||= Entry.select('entries.id, "entries"."guideKey", entries.name, entries.image, entries.bio, entries.entrytype, array_to_string(array_agg(requests.request), ''|'') as reqs').joins(:requests).where('"entries"."guideKey" = ?', params[:key]).group('entries.id')
+		@entry_requests ||= Entry.select('entries.id, "entries"."guideKey", entries.name, entries.image, entries.bio, entries.entrytype, array_to_string(array_agg(requests.request), E''|'') as reqs').joins(:requests).where('"entries"."guideKey" = ?', params[:key]).group('entries.id')
 	end
 
 	configure :development do
 		def entry_requests
-			@entry_requests ||= Entry.select('entries.id, "entries"."guideKey", entries.name, entries.image, entries.bio, entries.entrytype, group_concat(requests.request, ''|'') as reqs').joins(:requests).where('"entries"."guideKey" = ?', params[:key]).group('entries.id')
+			@entry_requests ||= Entry.select("entries.id, 'entries'.'guideKey', entries.name, entries.image, entries.bio, entries.entrytype, group_concat(requests.request, \"|\") as reqs").joins(:requests).where('"entries"."guideKey" = ?', params[:key]).group('entries.id')
 		end
 	end
 

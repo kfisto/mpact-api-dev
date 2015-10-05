d55c585d27dd66e4525b80b4021e5ae64ed319e5
Dan LaMee <dan.lamee@gmail.com>
2015-10-05 13:27:17 -0400

    convert string columns to text type

---
+++ db/migrate/20151005172337_update_bio_request_columns_type.rb
@@ -0,0 +1,12 @@
+class UpdateBioRequestColumnsType < ActiveRecord::Migration
+  def up
+    change_column :entries, :bio, :text
+    change_column :requests, :request, :text
+  end
+  def down
+    # This might cause trouble if you have strings longer
+    # than 255 characters.
+    change_column :entries, :bio, :string
+    change_column :requests, :request, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005011943) do
+ActiveRecord::Schema.define(version: 20151005172337) do
 
   create_table "entries", force: true do |t|
     t.string  "guideKey"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20151005011943) do
     t.string  "image"
     t.integer "entrytype"
     t.string  "location"
-    t.string  "bio"
+    t.text    "bio",       limit: 255
   end
 
   create_table "guides", force: true do |t|
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20151005011943) do
   end
 
   create_table "requests", force: true do |t|
-    t.string  "request"
+    t.text    "request",  limit: 255
     t.integer "entry_id"
   end
 
